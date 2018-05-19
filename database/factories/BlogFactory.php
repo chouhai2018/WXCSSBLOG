3f04346587d7fb54c201b262c0145035618bcdbe
ChouHai <makewongms@163.com>
2018-05-20 06:06:20 +0800

    重新修改模板

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -5,5 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\blog::class, function (Faker $faker) {
     return [
         //
+        'title' => $faker->text(20), //添加生成标题填充20个英文字符
+        'body' =>$faker->text, //添加内容，默认200个英文字符
     ];
 });
---
+++ tests/Feature/databaseeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class databaseeTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $users = factory(blog::class, 30)->create(); //在数据库中使用填充器创建30条数据
+        $this->assertTrue(true);
+    }
+}
